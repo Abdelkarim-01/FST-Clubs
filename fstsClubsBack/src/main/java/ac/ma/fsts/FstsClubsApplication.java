542e1554c110bc04b2074bf0c945c4cc3b8eebcd
Idrissi <idrissiabdelkarim01@gmail.com>
2024-11-04 20:20:56 +0100

    first commit

---
+++ fstsClubsBack/src/main/java/ac/ma/fsts/FstsClubsApplication.java
@@ -0,0 +1,13 @@
+package ac.ma.fsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstsClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstsClubsApplication.class, args);
+	}
+
+}
---
+++ fstsClubsBack/src/test/java/ac/ma/fsts/FstsClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.ma.fsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FstsClubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
